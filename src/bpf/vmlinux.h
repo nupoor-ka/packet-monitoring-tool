c7b300095d5e874d998fcf52f48cac0f2b437464
Aditya Kumar <120514790+Destructor169@users.noreply.github.com>
2025-10-25 15:03:02 +0530

    Add vmlinux.h placeholder
    This file should be generated from the kernel's BTF information.

---
+++ src/bpf/vmlinux.h
@@ -0,0 +1,10 @@
+/* Generated kernel type definitions */
+/* Generate this file using: bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h */
+
+#ifndef __VMLINUX_H__
+#define __VMLINUX_H__
+
+// This file should be generated from your kernel's BTF information
+// Run: bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+
+#endif /* __VMLINUX_H__ */
